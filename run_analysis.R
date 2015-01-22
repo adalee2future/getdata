5f739da7c41705146ee0afed7d071b476572ad9d
Ada Lee <adalee2future@users.noreply.github.com>
2015-01-22 15:42:18 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,6 @@
+# before running this you shoud download .zip
+# from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip,
+# and then unzip it as directory "UCI HAR Dataset", and then copy it to your r working directory
 # read features.txt, get all features
 features <- read.table("UCI HAR Dataset/features.txt", sep = " ", col.names = c("id", "feature"))
 X_names <- features$feature
